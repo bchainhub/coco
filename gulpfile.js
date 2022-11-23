da3082e18ace54a3b358ca9f6e3554914b222493
Rastislav <rastislav@coreblockchain.cc>
2022-11-23 10:07:04 +0100

    Involve Gulp

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var sassdoc = require('sassdoc');
+
+gulp.task('docs', function () {
+  return gulp.src('dist/sass/**/*.sass')
+    .pipe(sassdoc({
+      dest: 'docs/'
+    }));
+});
