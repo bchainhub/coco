31b1882d4ce491ae7d7a2c86e6abe4da0e9788c1
Rastislav <rastislav@coreblockchain.cc>
2022-11-30 14:28:57 +0100

    v2.1.0

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require('gulp');
 var sassdoc = require('sassdoc');
 
 gulp.task('docs', function () {
-  return gulp.src('dist/sass/**/*.sass')
-    .pipe(sassdoc({
-      dest: 'docs/'
-    }));
+	return gulp.src('dist/sass/**/*.sass')
+		.pipe(sassdoc({
+			dest: 'docs/'
+		}));
 });
