5668fcec593a0aa875da01603eae3f50e1fb508e
Rastislav <rastislav@onion.email>
2024-10-13 19:30:32 +0200

    Refactoring

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var sassdoc = require('sassdoc');
-
-gulp.task('docs', function () {
-	return gulp.src('dist/sass/**/*.sass')
-		.pipe(sassdoc({
-			dest: 'docs/'
-		}));
-});
